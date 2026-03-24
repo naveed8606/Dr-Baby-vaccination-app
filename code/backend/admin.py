a0fe1c83cc4f635cdfb7af961a3684ad55fa1e52
naveed nihan <139102750+naveed8606@users.noreply.github.com>
2026-03-25 00:20:32 +0530

    Register models in Django admin

---
+++ code/backend/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Child)
+admin.site.register(vaccine_names)
+admin.site.register(VaccinePrograms)
+admin.site.register(VaxName)
+admin.site.register(Vax_Cycle)
+admin.site.register(Vax)
+admin.site.register(Hospitals)
+admin.site.register(VaccineBooking)
